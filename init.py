9c72ff2929701dd8528195661fd6fc3fd49fd721
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-08-01 08:03:16 +0100

    Init file, to initialize project structure

---
+++ init.py
@@ -0,0 +1,19 @@
+directories = [
+    "configs",
+    "controllers",
+    "models",
+    "middlewares",
+    "routers",
+    "services",
+    "utils"
+]
+
+files = {
+    "configs": ["index"],
+    "controllers": ["userController", "authController", "index"],
+    "models": ["users"],
+    "middlewares": ["error", "auth"],
+    "routers": ["auth", "user", "index"],
+    "services": ["authService", "userService", "index"],
+    "utils": ["mail", "index"]
+}
